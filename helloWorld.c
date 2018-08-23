89533041f4b518f42b00a029a5f65cd71e669ddf
sifatsyed <42530377+sifatsyed@users.noreply.github.com>
2018-08-23 10:12:30 -0500

    Initial Commit

---
+++ helloWorld.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Sergio Carrera and Sifat Syed
+ * Date: 2018/08/21
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Name: Sifat Syed, Major:Computer Science\n");
+
+  return 0;
+}
